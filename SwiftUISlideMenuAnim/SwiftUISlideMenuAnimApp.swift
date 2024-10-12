1fd29f87a7a7493f178461772a69892b18e9f21e
manuaravind <manuaravindpta@gmail.com>
2024-10-12 11:59:25 +0530

    Initial Commit

---
+++ SwiftUISlideMenuAnim/SwiftUISlideMenuAnimApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISlideMenuAnimApp.swift
+//  SwiftUISlideMenuAnim
+//
+//  Created by Manu Aravind on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISlideMenuAnimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
